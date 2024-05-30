a26e87b647dfb0f0388e425793fccde5511e6f40
Ohenewaah09 <aoetsorblewu@st.ug.edu.gh>
2024-05-30 19:44:15 +0000

    create header section

--- SplashZone/App.js
+++ SplashZone/App.js
@@ -1,16 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.headerWrap}>
-        <Text style={styles.headerText}>Hello , Devs</Text>
-        <Text>14 task today</Text>
-        </View>
-      <Image source={require("./components/person.png")} style={styles.personImage}/>
-      </View>
+      <Header/>
+      <TextInput style={styles.searchBar} placeholder='Search' placeholderTextColor={'#888'}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,28 +17,7 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor: '#fff',
     alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    
-    
+    justifyContent: 'flex-start', 
   },
-  header:{
-    flex: 1,
-    backgroundColor: 'purple',
-    marginLeft: 20,
-    marginTop:52,
-    flexDirection: 'row'
-   
-  },
-  headerText:{
-    fontWeight: 'bold',
-    fontSize: 27
-  },
-  personImage:{
-    resizeMode: 'contain',
-    width: 46,
-    height: 45,
-    marginTop:52,
-    marginRight:10
-    
-  }
+  
 });
---
+++ SplashZone/components/header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+
+
+export default function Header() {
+    return(
+
+        <View style={styles.header}>
+           <View style={styles.headerWrap}>
+           <Text style={styles.headerText}>Hello, Devs</Text>
+            <Text style={styles.subHeaderText}>14 task today</Text>
+           </View>   
+      <Image source={require("./person.png")} style={styles.personImage}/>
+    </View>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    header:{
+        height:80,
+        backgroundColor: 'purple',
+        paddingTop:38,
+        marginTop:52,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        paddingHorizontal:10,
+        width:'100%',
+      },
+      headerText:{
+        fontWeight: 'bold',
+        fontSize: 27,
+      },
+      headerWrap:{
+        flexDirection:'column'
+      },
+      personImage:{
+        resizeMode: 'contain',
+        width: 46,
+        height: 45,
+        marginRight:10 
+      }
+})
